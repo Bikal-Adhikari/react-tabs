edb159d3e4821b05fc5d1cafab5793790cceacdc
Bikal <bikaladhikari5@gmail.com>
2024-04-21 17:39:27 +1000

    State value and fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect } from "react";
+import { useState } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {}, []);
   return <h2>Tabs Starter</h2>;
 };
 export default App;
